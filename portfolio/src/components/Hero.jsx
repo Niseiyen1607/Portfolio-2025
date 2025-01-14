4d76758e68de1bf3622b50231729256936de8081
Niseiyen <127323783+Niseiyen@users.noreply.github.com>
2025-01-14 09:42:08 -0500

    Creation de la section Hero

--- portfolio/src/components/Hero.jsx
+++ portfolio/src/components/Hero.jsx
@@ -1,10 +1,28 @@
+import profilPic from '../assets/images/placeholder/profile.png';
+
 const Hero = () => {
     return (
         <div className="border-b border-neutral-900 pb-4 lg:mb-35">
             <div className="flex flex-warp">
                 <div className="w-full lg:w-1/2">
                     <div className="flex flex-col items-center lg:items-start">
-                        <h1 className=""></h1>
+                        <h1 className="pb-16 text-6xl font-thin tracking-tight lg:mt-16
+                         lg:text-8xl">
+                            Thiraiyan Mooneesawmy
+                        </h1>
+                        <span className="bg-gradient-to-r from-pink-300 via-slate-500
+                        to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent">
+                            Computer Science Student
+                        </span>
+                        <p className="my-2 max-w-xl py-6 font-light">
+                            I am a Computer Science student at the college of Andre-Laurendeau
+                            and I am passionate about game development and web development.
+                        </p>
+                    </div>
+                </div>
+                <div className="w-full lg:w-1/2 lg:p-8">
+                    <div className="flex justify-center">
+                        <img src={profilPic} alt="Profile" className="rounded-full w-64 h-64" />
                     </div>
                 </div>
             </div>
